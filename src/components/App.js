e88a2d30ebbf19b7dd61d0856ac66257f49433f1
AbdiazizSaman <farhanadam394@gmail.com>
2023-10-25 18:20:40 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+
 
 function App() {
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  //  Event handler to toggle between dark and light modes
+  const toggleMode = () => {
+    console.log("Button clicked");
+    setIsDarkMode(!isDarkMode);
+  };
+
+  const appClass = isDarkMode ? "App dark" : "App light"
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        {/* Add an event handler to the button */}
+        <button onClick={toggleMode}>
+          {isDarkMode ? "Light Mode" : "Dark Mode"}
+        </button>
       </header>
       <ShoppingList items={itemData} />
     </div>
@@ -20,3 +32,4 @@ function App() {
 }
 
 export default App;
+
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false); // State to track whether the item is in the cart
+
+  const handleAddToCart = () => {
+    // Toggle the isInCart state when the button is clicked
+    setIsInCart(!isInCart);
+  };
+
   return (
-    <li className="">
+    <li className={isInCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAddToCart}>
+        {isInCart ? "Remove from Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
 
 export default Item;
+
